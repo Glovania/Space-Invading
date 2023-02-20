32eca27b9495688798fcb6dd16a84abad304eadc
Zefffie <9937412@schoolsnet.act.edu.au>
2023-02-20 14:44:05 +1100

    Update border

--- Player/Player.gd
+++ Player/Player.gd
@@ -23,8 +23,7 @@ func _physics_process(delta):
 		if position.x < 1250:
 			move_and_collide(Vector2(movement_speed * delta, 0))
 	if Input.is_action_pressed("ui_down"):
-		if position.x >30:
 			move_and_collide(Vector2(0, movement_speed * delta))
 	if Input.is_action_pressed("ui_up"):
-		if position.x <690:
+		if position.y >600:
 			move_and_collide(Vector2(0, -movement_speed * delta))
