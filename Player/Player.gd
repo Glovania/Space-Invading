06a9a8c12ceab7608852551b7faec8bc4aac6fd2
Zefffie <9937412@schoolsnet.act.edu.au>
2023-02-22 08:47:03 +1100

    Update ui_up borderline

--- Player/Player.gd
+++ Player/Player.gd
@@ -23,6 +23,7 @@ func _physics_process(delta):
 		if position.x < 1250:
 			move_and_collide(Vector2(movement_speed * delta, 0))
 	if Input.is_action_pressed("ui_down"):
+		if position.y <1690:
 			move_and_collide(Vector2(0, movement_speed * delta))
 	if Input.is_action_pressed("ui_up"):
 		if position.y >600:
