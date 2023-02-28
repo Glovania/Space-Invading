2f2e64e06ef4c460a784de05909eefc9d11bec17
Zefffie <9937412@schoolsnet.act.edu.au>
2023-03-01 09:32:38 +1100

    Updated "Timer" for MainGame.gd

--- Global.gd
+++ Global.gd
@@ -1,4 +1,4 @@
 extends Node
 
-var BulletInstanceCount = 0 #Keep track in the bullet numbers
-var enemyBulletInstanceCount = 0
+var BulletInstanceCount = 2 #Keep track in the bullet numbers
+var enemyBulletInstanceCount = 0 #Keep track in the enemy bullet numbers
--- MainGame/MainGame.gd
+++ MainGame/MainGame.gd
@@ -7,8 +7,15 @@ var currentTimer
 # Called when the node enters the scene tree for the first time.
 
 func _ready():
-	set_process(true)
-	set_physics_process(true)
+	currentTimer = countdownMax
+	
+	while currentTimer > 0:
+		yield(get_tree().create_timer(1.0), "timeout")
+		$HUD/Countdown.text = str(currentTimer)
+		currentTimer = currentTimer - 1
+		print(currentTimer)
+	print("Game Over")
+	
 	
 	var name = "Autobot 1"
 	var welcome = "Allios"
