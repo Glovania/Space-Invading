f9d8d8495da1a630bfe9017f4b982bd813b4a2eb
Glovania <9937412@schoolsnet.act.edu.au>
2023-04-25 15:50:52 +1000

    Update 'Main Menu' & 'Main Game'
    Adding Background for a better UI.

---
+++ Menu/MainMenu.gd
@@ -0,0 +1,30 @@
+extends Control
+
+
+export var MainGameScene: PackedScene
+
+
+func _ready():
+	$MarginContainer/Layout/Main/ButtonHolder/StartGame.grab_focus()
+	
+#	for button in $Layout/Main/Buttons/GameScene/Quit.get_children():
+#		button.connect("pressed", self, "_on_Button_pressed", [button.scene_to_load])
+
+#func loadData():
+#	var file = File.new()
+#	if file.file_exists(GlobalVariables.Filesave):
+#		var error = file.open(GlobalVariables.Filesave, File.READ)
+#		if error == OK:
+#			var player_data = file.get_var()
+#			file.close()
+#			GlobalVariables.ScroingInformation = player_data
+
+
+func _on_Start_Game_Button_pressed():
+	get_tree().change_scene("res://MainGame/MainGame.tscn")
+	
+func _on_Options_pressed():
+	get_tree().change_scene("res://Menu/Option.tscn")
+	
+func _on_Quit_pressed():
+	get_tree().quit()
--- Menu/Options.gd
+++
@@ -1,9 +0,0 @@
-extends Button
-
-
-func _ready():
-	pass # Replace with function body.
-
-
-func _on_Options_pressed():
-	get_tree().change_scene("res://Menu/Option.tscn")
