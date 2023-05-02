73cfa1fe757afcb82ce8487cf5a82bef44b45cba
Glovania <9937412@schoolsnet.act.edu.au>
2023-05-03 09:31:50 +1000

    🐞 fix: Adding PlayerHealth
    Still ongoing.

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,18 +1,10 @@
 extends KinematicBody2D
 
 
-signal health_updated(health)
-signal killed()
-
-export(int) var MaxHealth = 20
-
-onready var health_bar = $"Control/HP Bar"
-# onready var invulnerability_timer = $Area2D/InvulnerabilityTimer
-# onready var animation = $AnimationPlayer
-
 var movement_speed = 250
 var bulletSource = preload("res://Bullet/Bullet.tscn")
 
+onready var health = $HP_Bar/HP_Percentage
 
 func _ready():
 	set_process(true)
@@ -29,12 +21,9 @@ func _process(delta):
 			var bulletInstance = bulletSource.instance()
 			bulletInstance.position = Vector2(position.x, position.y-88)
 			get_tree().get_root().add_child(bulletInstance)
+			
+	$HP_Bar/HP_Percentage.value = health
 
-func _on_heath_updated(health, amount):
-	health_bar.value = health
-
-func _on_max_health_updated(max_heath):
-	health_bar.max_value = max_heath
 	
 func _physics_process(delta):
 	if Input.is_action_pressed("ui_left"):
@@ -51,19 +40,3 @@ func _physics_process(delta):
 	if Input.is_action_pressed("ui_up"):
 		if position.y > 600:
 			move_and_collide(Vector2(0, -movement_speed * delta))
-
-#func damage(amount):
-#	if invulnerability_timer.is_stopped():
-#		invulnerability_timer.start()
-#	_setHealth(health - amount)
-
-#func kill():
-#	pass 
-#	
-#func _setHealth(value):
-#	var previousHealth = health
-#	health = clamp(value, 0, MaxHealth)
-#	if health != previousHealth:
-#		emit_signal("health_updated", health)
-#		if health == 0:
-#			kill()
