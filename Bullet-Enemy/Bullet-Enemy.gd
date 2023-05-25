346a74d5aa6a1cfd864d100ac95116a230933b0c
Glovania <9937412@schoolsnet.act.edu.au>
2023-05-25 21:26:49 +1000

    :zap: Update 'HealthBar' feature
    - Fix player get killed instantly after 1 shot despite having 5 hearts.

--- Bullet-Enemy/Bullet-Enemy.gd
+++ Bullet-Enemy/Bullet-Enemy.gd
@@ -3,7 +3,6 @@ extends KinematicBody2D
 
 var speed = 500
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	GlobalVariables.enemyBulletInstanceCount += 1
@@ -13,14 +12,11 @@ func _physics_process(delta):
 	var collidedObject = move_and_collide(Vector2(0, +speed * delta * 0.4))
 	if (collidedObject):
 		# print("Enemy collide ",collidedObject.collider.name)
-		if "Enemy" in collidedObject.collider.name:
-			pass
-		else:
-			queue_free()
-			GlobalVariables.enemyBulletInstanceCount -= 1
-			
+			if "Enemy" in collidedObject.collider.name:
+				pass
+				GlobalVariables.enemyBulletInstanceCount -= 1
+				
 			if "Player" in collidedObject.collider.name:
 				collidedObject.get_collider().queue_free()
 				get_tree().change_scene("res://MainGame/LoseScene.tscn")
-			else:
 				print("Enemy Bullet: ", GlobalVariables.enemyBulletInstanceCount)
--- Player/Player.gd
+++ Player/Player.gd
@@ -1,16 +1,19 @@
 extends KinematicBody2D
 
-
-export (int) var movement_speed: int = 350
-export (int) var max_heath
-
-
+var health = 10
+var movement_speed = 350
 var bulletSource = preload("res://Bullet/Bullet.tscn")
 
 func _ready():
-	
+
 	set_process(true)
 	set_physics_process(true)
+
+
+func _physics_process(delta):
+
+	playerMovement(delta)
+
 
 func _process(delta):
 	if GlobalVariables.AutoFiring:
@@ -23,22 +26,23 @@ func _process(delta):
 			var bulletInstance = bulletSource.instance()
 			bulletInstance.position = Vector2(position.x, position.y-88)
 			get_tree().get_root().add_child(bulletInstance)
-			
-# 	$HP_Bar/HP_Percentage.value = health
 
-	
-func _physics_process(delta):
-	if Input.is_action_pressed("ui_left"):
-		if position.x > 30:
-#			animation.play("run")
-			move_and_collide(Vector2(-movement_speed * delta, 0))
-	if Input.is_action_pressed("ui_right"):
-		if position.x < 1250:
-#			animation.play("run")
-			move_and_collide(Vector2(movement_speed * delta, 0))
-	if Input.is_action_pressed("ui_down"):
-		if position.y < 1690:
-			move_and_collide(Vector2(0, movement_speed * delta))
-	if Input.is_action_pressed("ui_up"):
-		if position.y > 600:
-			move_and_collide(Vector2(0, -movement_speed * delta))
+
+func playerMovement(delta):
+		if Input.is_action_pressed("ui_left"):
+			if position.x > 30:
+				move_and_collide(Vector2(-movement_speed * delta, 0))
+		if Input.is_action_pressed("ui_right"):
+			if position.x < 1250:
+				move_and_collide(Vector2(movement_speed * delta, 0))
+		if Input.is_action_pressed("ui_down"):
+			if position.y < 1690:
+				move_and_collide(Vector2(0, movement_speed * delta))
+		if Input.is_action_pressed("ui_up"):
+			if position.y > 600:
+				move_and_collide(Vector2(0, -movement_speed * delta))
+
+func update_heath():
+	var health_bar = $HealthBar
+	health_bar.value = health 
+	health_bar.visible = false
