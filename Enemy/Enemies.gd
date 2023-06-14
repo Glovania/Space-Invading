166b50d93f4308f48eb178187238ac2d13210519
Glovania <9937412@schoolsnet.act.edu.au>
2023-06-14 10:05:04 +1000

    🚅 perfs: Re-order codes.

--- Enemy/Enemies.gd
+++ Enemy/Enemies.gd
@@ -5,8 +5,12 @@ var speed = 170
 
 func _ready():
 	set_physics_process(true)
-	
-	
+
+
 func _physics_process(delta):
+	enemyMovement(delta)
+
+
+func enemyMovement(delta):
 	global_position.x += speed * delta
 	pass 
